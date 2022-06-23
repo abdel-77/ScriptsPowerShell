2c3d8573facb5bd8a546091f0c001a75e295bd04
abdel-77 <62035298+abdel-77@users.noreply.github.com>
2022-06-23 19:43:55 +0200

    Add files via upload

---
+++ Robocopy-Documents.ps1
@@ -0,0 +1,24 @@
+﻿# Script de sauvegarde de la directory Documents vers HDD EXT
+$DirLog = "C:\Tools\log\"
+$Date = Get-Date -Format ddMMyyyy
+$FicLog = "$Date-Copylog.txt"
+$DirSource = "D:\Documents\"
+$DirDest = "F:\Documents\"
+# $DirSource = "D:\"
+# $DirDest = "F:\SAVE-D\"
+
+ADD-content -path $DirLog\$FicLog -value (Get-Date)
+ADD-content -path $DirLog\$FicLog -value "Debut de sauvegarde" 
+
+if(Test-Path $DirDest){
+   ADD-content -path $DirLog\$FicLog -value "Disque Externe Disponible" ; 
+   robocopy $DirSource $DirDest  /MIR /NFL /NDL /Z /R:3 /W:5 /UNILOG+:$DirLog$FicLog ;
+   ADD-content -path $DirLog\$FicLog -value (Get-Date) ;
+   ADD-content -path $DirLog\$FicLog -value "Fin de sauvegarde"
+}
+else{
+   ADD-content -path $DirLog\$FicLog -value "Disque F non Disponible !!"
+}
+
+
+
